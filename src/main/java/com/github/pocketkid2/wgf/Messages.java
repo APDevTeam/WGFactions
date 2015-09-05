e01cfce41d410eed12654cd51178d8d1ac5b41a6
Adam <afranklint16@gmail.com>
2015-09-05 15:14:32 -0700

    Dangit, I missed this file in the last commit

--- src/main/java/com/github/pocketkid2/wgf/Messages.java
+++ src/main/java/com/github/pocketkid2/wgf/Messages.java
@@ -9,7 +9,7 @@ public interface Messages {
 	String INVALID_WORLD = ChatColor.RED + "Invalid world!";
 	String INVALID_REGION = ChatColor.RED + "Invalid region!";
 	String INVALID_FACTION = ChatColor.RED + "Invalid faction!";
-	String ADDED = ChatColor.AQUA + "Added all players from faction " + ChatColor.GREEN + "%s " + ChatColor.AQUA + "to region " + ChatColor.BLUE + "%s";
+	String ADDED = ChatColor.AQUA + "Added %s from faction " + ChatColor.GREEN + "%s " + ChatColor.AQUA + "to region " + ChatColor.BLUE + "%s";
 	String REMOVED = ChatColor.AQUA + "Removed all players from faction " + ChatColor.GREEN + "%s " + ChatColor.AQUA + "from region " + ChatColor.BLUE + "%s";
 	String INVALID_MODE = ChatColor.RED + "Invalid add mode! Valid modes are OFFICER, MEMBER, or ALL";
 
